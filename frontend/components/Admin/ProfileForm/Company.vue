80e9283b6d077e940f0d302bcc669797fb60f2a0
unknown <rostik057@gmail.com>
2024-08-06 15:58:54 +0300

    !wip company

---
+++ frontend/components/Admin/ProfileForm/Company.vue
@@ -0,0 +1,38 @@
+<template>
+  <AdminFormBlock title="Car photo">
+    <div class="form-item">
+      <VFormComponent :field="transmission_id" />
+      <VFormComponent :field="colour_id" />
+    </div>
+  </AdminFormBlock>
+</template>
+
+<script setup>
+const props = defineProps({
+  user: Object,
+});
+
+const company_name = ref({
+  type: "text",
+  name: "company.name",
+  rules: "required",
+  modelValue: props?.user?.company?.name ?? "",
+
+  bind: {
+    label: "Company Name",
+    placeholder: "My Company",
+  },
+});
+
+const company_trn_number = ref({
+  type: "text",
+  name: "company.company_trn_number",
+  rules: "required",
+  modelValue: props?.user?.company?.company_trn_number ?? "",
+
+  bind: {
+    label: "TRN number (TAX)",
+    placeholder: "TRN number (TAX)",
+  },
+});
+</script>
---
+++ frontend/components/Admin/ProfileForm/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="profile-form">
+    <div class="profile-form__left">
+      <AdminProfileFormCompany />
+    </div>
+    <div class="profile-form__right"></div>
+  </div>
+</template>
