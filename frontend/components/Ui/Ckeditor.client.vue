1058af72da05389306de906c0d51766884ee2e82
Rostislav <rostik057@gmail.com>
2024-09-10 18:14:57 +0300

    !fix

--- frontend/components/Ui/Ckeditor.client.vue
+++ frontend/components/Ui/Ckeditor.client.vue
@@ -6,7 +6,10 @@
 const CKEditor = await import("@ckeditor/ckeditor5-vue");
 // import CKEditor from "@ckeditor/ckeditor5-vue";
 // const ClassicEditor = await import("@ckeditor/ckeditor5-build-classic");
-import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+// import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+const ClassicEditor = await import("@ckeditor/ckeditor5-build-classic").then(
+  (res) => res.default
+);
 import type { EditorConfig } from "@ckeditor/ckeditor5-core";
 
 const ckedit = CKEditor.default.component;
