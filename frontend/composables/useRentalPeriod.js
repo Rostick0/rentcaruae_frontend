082aa5b3d06af7bbcecd78481fcd44f237a2d092
Rostislav <rostik057@gmail.com>
2024-09-10 17:09:01 +0300

    !fix ckeditor

--- frontend/composables/useRentalPeriod.js
+++ frontend/composables/useRentalPeriod.js
@@ -1,31 +1,39 @@
 export default () => {
+  const setRentalPeriods = (cityName) => [
+    {
+      name: "Daily rental",
+      link: `/${cityName}/daily`,
+    },
+    {
+      name: "Weekly rental",
+      link: `/${cityName}/weekly`,
+    },
+    {
+      name: "Monthly rental",
+      link: `/${cityName}/monthly`,
+    },
+    {
+      name: "Car leasing",
+      link: `/${cityName}/leasing`,
+    },
+  ];
+
   const currentCity = useState("currentCity");
 
   const rentalPeriods = useState("rentalPeriods", () => []);
 
+  if (currentCity.value?.name) {
+    rentalPeriods.value = setRentalPeriods(
+      currentCity.value?.name?.toLowerCase?.()
+    );
+  }
+
   watch(
     () => currentCity.value,
     (newV) => {
-      const cityName = newV?.name?.toLowerCase();
+      if (!newV?.name) return;
 
-      rentalPeriods.value = [
-        {
-          name: "Daily rental",
-          link: `/${cityName}/daily`,
-        },
-        {
-          name: "Weekly rental",
-          link: `/${cityName}/weekly`,
-        },
-        {
-          name: "Monthly rental",
-          link: `/${cityName}/monthly`,
-        },
-        {
-          name: "Car leasing",
-          link: `/${cityName}/leasing`,
-        },
-      ];
+      rentalPeriods.value = setRentalPeriods(newV?.name?.toLowerCase?.());
     }
   );
 
