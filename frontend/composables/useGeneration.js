7012e9e48ead6b796ed652a45bb79627d0150c35
Rostislav <rostik057@gmail.com>
2024-09-16 10:31:55 +0300

    !fix

--- frontend/composables/useGeneration.js
+++ frontend/composables/useGeneration.js
@@ -1,36 +1,7 @@
 // import api from "~/api";
 
 export default async () => {
-  const generations = useState("generations", () => [
-    {
-      name: "SUV",
-      img: "frontend-images/car-type/SUV.png",
-    },
-    {
-      name: "VAN",
-      img: "frontend-images/car-type/VAN.png",
-    },
-    {
-      name: "Sedan",
-      img: "frontend-images/car-type/Sedan.png",
-    },
-    {
-      name: "Coupe",
-      img: "frontend-images/car-type/Coupe.png",
-    },
-    {
-      name: "Cabrio",
-      img: "frontend-images/car-type/Cabrio.png",
-    },
-    {
-      name: "Hatchback",
-      img: "frontend-images/car-type/Hatchback.png",
-    },
-    {
-      name: "Liftback",
-      img: "frontend-images/car-type/Liftback.png",
-    },
-  ]);
+  const generations = useState("generations", () => bodyTypesWithIcon);
 
   return {
     generations,
--- frontend/utils/bodyTypes.js
+++ frontend/utils/bodyTypes.js
@@ -1,30 +1,30 @@
 export const bodyTypesWithIcon = [
   {
     name: "SUV",
-    img: "frontend-images/car-type/SUV.png",
+    img: "/frontend-images/car-type/SUV.png",
   },
   {
     name: "VAN",
-    img: "frontend-images/car-type/VAN.png",
+    img: "/frontend-images/car-type/VAN.png",
   },
   {
     name: "Sedan",
-    img: "frontend-images/car-type/Sedan.png",
+    img: "/frontend-images/car-type/Sedan.png",
   },
   {
     name: "Coupe",
-    img: "frontend-images/car-type/Coupe.png",
+    img: "/frontend-images/car-type/Coupe.png",
   },
   {
     name: "Cabrio",
-    img: "frontend-images/car-type/Cabrio.png",
+    img: "/frontend-images/car-type/Cabrio.png",
   },
   {
     name: "Hatchback",
-    img: "frontend-images/car-type/Hatchback.png",
+    img: "/frontend-images/car-type/Hatchback.png",
   },
   {
     name: "Liftback",
-    img: "frontend-images/car-type/Liftback.png",
+    img: "/frontend-images/car-type/Liftback.png",
   },
 ];
