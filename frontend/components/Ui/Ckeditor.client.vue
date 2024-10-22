8a71248321d38895db494f0b77ca26b41defdc84
Rostislav <rostik057@gmail.com>
2024-10-22 16:03:41 +0300

    !fix

--- frontend/components/Ui/Ckeditor.client.vue
+++ frontend/components/Ui/Ckeditor.client.vue
@@ -1,5 +1,10 @@
 <template>
-  <ckedit :editor="ClassicEditor" :config="editorConfig" v-model="value" />
+  <ckedit
+    :editor="ClassicEditor"
+    :config="editorConfig"
+    v-model="value"
+    @ready="onInit"
+  />
 </template>
 
 <script lang="ts" setup>
@@ -11,18 +16,54 @@ const ClassicEditor = await import("@ckeditor/ckeditor5-build-classic").then(
   (res) => res.default
 );
 import type { EditorConfig } from "@ckeditor/ckeditor5-core";
+import api from "~/api";
 
 const ckedit = CKEditor.default.component;
+
+const config = useRuntimeConfig();
 
 const props = defineProps<{
   modelValue?: any;
 }>();
 const emit = defineEmits(["update:modelValue"]);
 
+class UploadAdapter {
+  constructor(loader: any) {
+    this.loader = loader;
+  }
+
+  async upload() {
+    return this.loader.file.then((file: any) => {
+      const data = new FormData();
+      data.append("image", file);
+      const genericError = `Couldn't upload file: ${file.name}.`;
+
+      return api.image
+        .create({
+          data,
+        })
+        .then((res) => {
+          console.log(res);
+          return { default: res?.data.path_webp };
+        })
+        .catch(({ error }) => Promise.reject(error?.message ?? genericError));
+    });
+  }
+
+  abort() {
+    return Promise.reject();
+  }
+}
+
+const onInit = (editor: any) => {
+  editor.plugins.get("FileRepository").createUploadAdapter = (loader: any) =>
+    new UploadAdapter(loader);
+};
+
 const editorConfig = ref<EditorConfig>({
   // ckfinder: {
-  //     uploadUrl: '/api/file/upload'
-  // }
+  //   uploadUrl: config.public.BACK_URL + "/api/image/upload",
+  // },
 });
 
 const value = computed({
