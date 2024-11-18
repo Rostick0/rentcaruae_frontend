fa5f851665672f80a5bdd782aa0a828760e5c79b
Rostislav <rostik057@gmail.com>
2024-11-18 20:13:17 +0300

    !fix

--- frontend/pages/blogs/[title_link].vue
+++ frontend/pages/blogs/[title_link].vue
@@ -2,14 +2,14 @@
   <div class="blog-once">
     <div class="container">
       <Breadcrumbs :breadcrumbs="breadcrumbs" />
-      <Post :post="post" />
-      <div class="blog-once-sumilar" v-if="similarPosts?.length">
+      <Post :post="data" />
+      <div class="blog-once-sumilar" v-if="posts?.length">
         <TitleList
-          :link="`/blogs?filterEQ[post_category_id]=${post?.post_category_id}`"
+          :link="`/blogs?filterEQ[post_category_id]=${data?.post_category_id}`"
           linkText="All news"
           title="More news"
         ></TitleList>
-        <PostsList :posts="similarPosts" />
+        <PostsList :posts="posts" />
       </div>
     </div>
   </div>
@@ -19,60 +19,49 @@
 const route = useRoute();
 const config = useRuntimeConfig();
 
-// Реактивные данные
-const post = ref(null);
-const similarPosts = ref([]);
+const { data, get } = await useApi({
+  name: "posts.getByTitleLink",
+  params: {
+    extends:
+      "image.image,user,car.images.image,car.generation.model_car.brand,car.price,car.fuel_type,car.transmission,car.price_special,car.security_deposit",
+  },
+
+  requestParams: {
+    title_link: route.params?.title_link,
+  },
+});
+await get();
+
+const { data: posts, get: getPosts } = await useApi({
+  name: "posts.getAll",
+  params: {
+    extends: "image.image",
+    // "filterEQ[post_category_id]": data.value?.post_category_id,
+    "filterEQ[is_show]": 1,
+    limit: 4,
+  },
+});
+await getPosts();
+
 const breadcrumbs = [
   { name: "Home", link: "/" },
   { name: "Blog", link: "/blogs" },
-  { name: post.value?.title || "Post" },
+  {
+    name: data.value?.title,
+  },
 ];
 
-// Функция для загрузки данных
-async function fetchData() {
-  try {
-    const [postResponse, postsResponse] = await Promise.all([
-      useApi({
-        name: "posts.getByTitleLink",
-        params: {
-          extends:
-            "image.image,user,car.images.image,car.generation.model_car.brand,car.price,car.fuel_type,car.transmission,car.price_special,car.security_deposit",
-        },
-        requestParams: {
-          title_link: route.params?.title_link,
-        },
-      }),
-      useApi({
-        name: "posts.getAll",
-        params: {
-          extends: "image.image",
-          "filterEQ[is_show]": 1,
-          limit: 4,
-        },
-      }),
-    ]);
+useHead({
+  script: [getPostSchema(data.value)],
+  link: [
+    {
+      rel: "amphtml",
+      href: config.public.BASE_URL + "/amp" + route.path,
+    },
+  ],
+});
 
-    post.value = await postResponse.get();
-    similarPosts.value = await postsResponse.get();
-
-    // Настройка SEO
-    useSeoMeta(getPostSeoMeta(post.value));
-    useHead({
-      script: [getPostSchema(post.value)],
-      link: [
-        {
-          rel: "amphtml",
-          href: config.public.BASE_URL + "/amp" + route.path,
-        },
-      ],
-    });
-  } catch (error) {
-    console.error("Error fetching data:", error);
-  }
-}
-
-// Загрузка данных
-await fetchData();
+useSeoMeta(getPostSeoMeta(data.value));
 </script>
 
 <style lang="scss" scoped>
